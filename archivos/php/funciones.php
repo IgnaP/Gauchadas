2dcde2298ed9e9bc5e03481264606d1e96ef2f93
Ignacio <ignacioposada@hotmail.com>
2017-05-28 23:10:31 -0300

    Add files via upload

---
+++ archivos/php/funciones.php
@@ -0,0 +1,14 @@
+<?php
+function subirImagen(&$nombre_imagen,&$respuesta){
+  $nombre_imagen=$_FILES["imagen"]["name"];
+  $tipo_imagen=$_FILES["imagen"]["type"];
+  if ($tipo_imagen=="image/jpg" || $tipo_imagen=="image/png" || $tipo_imagen=="image/jpeg") {
+    $carpeta_destino='imagenes/';
+    move_uploaded_file($_FILES['imagen']['tmp_name'],$carpeta_destino.$nombre_imagen);
+    return true;
+  } else {
+    $respuesta="Solo se pueden subir imagenes: jpg/jpeg/png";
+    return false;
+  }
+}
+?>
