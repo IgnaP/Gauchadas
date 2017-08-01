067764b93a4dc44d84a0a9a8f7407cd1315c4cfe
komartinez <kottymartinez@gmail.com>
2017-08-01 13:53:00 -0300

    arreglos en lista usuarios

--- archivos/listaUsuarios.php
+++ archivos/listaUsuarios.php
@@ -3,7 +3,7 @@
   	conectarse($conexion);
   	session_start();
 
-  	$sql = "SELECT `ID`,`Nombre`, `Apellido` FROM `usuarios` WHERE `Administrador` = '0' ORDER BY `Apellido`, `Nombre`";
+  	$sql = "SELECT `ID`,`Nombre`, `Apellido`, `Email` FROM `usuarios` WHERE `Administrador` = '0' ORDER BY `Apellido`, `Nombre`";
   	$resultado = mysqli_query($conexion,$sql);
   	$rows = mysqli_num_rows($resultado);
 ?>
@@ -25,12 +25,25 @@
      <?php if($rows > 0){ ?>
 		<div class="row fondoBlanco col-md-12">
 			<div><h4>Usuarios totales: <?php echo "$rows" ?></h4></div>
+			<div class="row separar text-justify" style="margin-left: 1px; margin-right: 1px; color: white">
+				<div class="col-md-4" style="background-color:#6DBDD6;">
+					<div><strong>Apellido</strong></div>
+				</div>
+				<div class="col-md-4" style="background-color:#6DBDD6;">
+					<div><b>Nombre</b></div>
+				</div>
+				<div class="col-md-4" style="background-color:#6DBDD6;">
+					<div><strong>Email</strong></div>
+				</div>
+			</div>
 			<?php while($usuarios = mysqli_fetch_row($resultado)){
 				$u = $usuarios;
 			?>
 			<div class="row separar text-justify"> 
 				<div class="text-justify" style="margin-left: 15px; margin-right: 15px; border-width: 2px; border-color: #ECECEA; border-bottom-style: solid;">
-						<?php echo "$u[2]"." "."$u[1]" ?>
+					<div class="col-md-4 text-justify"><?php echo "$u[2]" ?></div>
+					<div class="col-md-4 text-justify"><?php echo "$u[1]" ?></div>
+					<div class="col-md-4 text-justify"><?php echo "$u[3]" ?></div>
 				</div>
 			</div>
 			<?php } ?>
